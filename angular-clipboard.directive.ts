00e89b45a5982d82608390fe1b783ec18be46092
Belaroth <rsylvester13@gmail.com>
2017-02-17 15:05:36 -0600

    angular 2

---
+++ angular-clipboard.directive.ts
@@ -0,0 +1,38 @@
+﻿import { Directive, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
+
+import { AngularClipboardProvider } from './angular-clipboard.provider';
+
+
+@Directive({
+    selector: '[clipboard]'
+})
+export class AngularClipboardDirective {
+    constructor(private el: ElementRef,
+        private clipboard: AngularClipboardProvider
+    ) {
+        this.supported = clipboard.supported;
+    }
+
+    public supported: boolean;
+
+    @Input() text: string;
+    @Output() onCopied = new EventEmitter();
+    @Output() onError = new EventEmitter();
+
+    @HostListener('click') onclick() {
+        try {
+            this.clipboard.copyText(this.text, this.el);
+            if (this.onCopied.observers.length > 0) {
+                this.onCopied.emit(null);
+            }
+        } catch (err) {
+            if (this.onError.observers.length > 0) {
+                this.onError.emit({ err: err });
+            }
+        }
+    }
+}
+//@Component({
+//    selector: 'note-summary',
+//    templateUrl: './note-summary.component.html'
+//})
---
+++ angular-clipboard.provider.ts
@@ -0,0 +1,53 @@
+﻿import { Directive, Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+
+
+@Injectable()
+export class AngularClipboardProvider {
+    public supported: boolean;
+
+    constructor( @Inject(DOCUMENT) private document: any) {
+        this.supported = document.queryCommandSupported('copy')
+    }
+
+    public createNode(text: string): HTMLElement {
+        var node = document.createElement('textarea');
+        node.style.position = 'absolute';
+        node.textContent = text;
+        node.style.left = '-10000px';
+        node.style.top = (window.pageYOffset || document.documentElement.scrollTop) + 'px';
+        return node;
+    }
+
+    public copyNode(node: HTMLElement): void {
+        try {
+            // Set inline style to override css styles
+            document.body.style.webkitUserSelect = 'initial';
+
+            var range = document.createRange();
+            range.selectNodeContents(node);
+            var selection = document.getSelection();
+            selection.removeAllRanges();
+            selection.addRange(range);
+
+            if (!document.execCommand('copy')) {
+                throw ('failure copy');
+            }
+        } finally {
+            // Reset inline style
+            document.body.style.webkitUserSelect = '';
+        }
+    }
+
+    public copyText(text: string, scope?: any) {
+        var node = this.createNode(text);
+        document.body.appendChild(node);
+        this.copyNode(node);
+        document.body.removeChild(node);
+    }
+
+    //return {
+    //    copyText: copyText,
+    //    supported: 'queryCommandSupported' in $document[0] && $document[0].queryCommandSupported('copy')
+    //};
+}
